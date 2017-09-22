3c79a24c7e66d576ed46c88a6bb980943c1ddfdc
GersenJPJorge <gersen@ibest.com.br>
2017-09-22 19:05:53 -0300

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MeuPrimeiroComponente_VSCode } from './app.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [MeuPrimeiroComponente_VSCode],
+    bootstrap: [MeuPrimeiroComponente_VSCode]
+})
+export class AppModule { }  
